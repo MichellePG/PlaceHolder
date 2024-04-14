1e93b7d4cbe363bf33196a41a92f2f85fabea4d5
Miliwi <milanjael@hotmail.com>
2024-04-14 15:02:09 -0400

    Salto del personaje
    Se añade salto entre movimientos.
    Al ajustar la gravedad, SPEED y JUMP_SPEED, se ajusta la dificultad mediante el tiempo de reacción necesario para jugar.

--- scenes/personaje.gd
+++ scenes/personaje.gd
@@ -1,13 +1,13 @@
 extends CharacterBody3D
 
 
-const SPEED = 1
-const JUMP_VELOCITY = 4.5
+const SPEED = 2.0
+const JUMP_VELOCITY = 5.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
-var timer = 0.0
+
 var move_direction
 
 func _physics_process(delta):
@@ -16,22 +16,22 @@ func _physics_process(delta):
 		velocity.y -= gravity * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	#if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	#	velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 
 	# Move the character automatically every 3 seconds
-	timer += delta
-	if timer > 1.0:
+	if is_on_floor():
 		# Choose a random direction
-		var directions = [Vector3(2, 0, 0), Vector3(-2, 0, 0), Vector3(0, 0, 1), Vector3(0, 0, -2)]
+		var directions = [Vector3(1, 0, 0), Vector3(-1, 0, 0), Vector3(0, 0, 1), Vector3(0, 0, -1)]
 		var random_dir = directions[randi() % directions.size()]
 		move_direction = random_dir
-		timer = 0.0
 		print(move_direction)
-	
+		
+		# Jump before moving
+		velocity.y = JUMP_VELOCITY
 	# Move the character in the chosen direction
 	if move_direction:
 		velocity.x = move_direction.x * SPEED
