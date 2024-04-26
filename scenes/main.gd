4ee327d6879a0830c0eb96c9ae7deeb1c2369687
Miliwi <milanjael@hotmail.com>
2024-04-25 20:40:49 -0400

    Game over chanta
    Añadió pantalla game over. Igual falta un boton de retry

--- scenes/main.gd
+++ scenes/main.gd
@@ -2,24 +2,35 @@ extends Node3D
 
 # Importa la definición de PlataformaBase
 const PlataformaBase = preload("res://scenes/plataforma_base.gd")
-var plataforma_base: PlataformaBase
+@onready var plataforma_base: PlataformaBase  = PlataformaBase.new()
+var cursor : Vector3 = Vector3(0, 0, -2)
+@onready var personaje = $Personaje
+
 
 func _ready():
-	plataforma_base = PlataformaBase.new()
 	add_child(plataforma_base)
+	plataforma_base.crear_nueva_plataforma(Vector3(0,0,0))
+	#var cursor : Vector3 = Vector3(0,0,0)
 
 func _input(event):
 	if event.is_action_pressed("move_right"):
-		crear_plataforma(Vector3(-2, 0, 0))
+		plataforma_base.crear_plataformas_invisibles(Vector3(-2, 0, 0))
+		cursor = Vector3(-2, 0, 0)
+		#plataforma_base.crear_nueva_plataforma(Vector3(-2, 0, 0))
 	elif event.is_action_pressed("move_left"):
-		crear_plataforma(Vector3(2, 0, 0))
+		plataforma_base.crear_plataformas_invisibles(Vector3(2, 0, 0))
+		cursor = Vector3(2, 0, 0)
 	elif event.is_action_pressed("move_forward"):
-		crear_plataforma(Vector3(0, 0, -2))
+		plataforma_base.crear_plataformas_invisibles(Vector3(0, 0, -2))
+		cursor = Vector3(0, 0, -2)
 	elif event.is_action_pressed("move_backward"):
-		crear_plataforma(Vector3(0, 0, 2))
+		plataforma_base.crear_plataformas_invisibles(Vector3(0, 0, 2))
+		cursor = Vector3(0, 0, 2)
+	elif  event.is_action_pressed("place_platform"):
+		plataforma_base.crear_nueva_plataforma(cursor)
+		plataforma_base.crear_plataformas_invisibles(cursor)
 
-func crear_plataforma(direccion: Vector3):
-	#plataforma_base.crear_nueva_plataforma(direccion)
-	var nueva_plataforma = plataforma_base.crear_nueva_plataforma(direccion)
-
+func _process(_delta):
+	if personaje.velocity.x == 0 and personaje.velocity.y == 0 and personaje.velocity.z == 0 and personaje.global_position.y <-0.5:
+		get_tree().change_scene_to_file("res://scenes/game_over_screen.tscn")
 	
--- scenes/personaje.gd
+++ scenes/personaje.gd
@@ -1,6 +1,5 @@
 extends CharacterBody3D
 
-
 const SPEED = 2.0
 const JUMP_VELOCITY = 5.0
 
@@ -11,33 +10,36 @@ var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 var move_direction
 
 func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y -= gravity * delta
+	
+	if global_position.y >-1.0:
+		# Add the gravity.
+		if not is_on_floor():
+			velocity.y -= gravity * delta
 
-	# Handle jump.
-	#if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-	#	velocity.y = JUMP_VELOCITY
+		# Get the input direction and handle the movement/deceleration.
+		# As good practice, you should replace UI actions with custom gameplay actions.
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+		if is_on_floor() :
+			# Choose a random direction
+			var directions = [Vector3(1, 0, 0), Vector3(-1, 0, 0), Vector3(0, 0, 1), Vector3(0, 0, -1)]
+			var random_dir = directions[randi() % directions.size()]
+			move_direction = random_dir
+			print(move_direction)
+			
+			# Jump before moving
+			velocity.y = JUMP_VELOCITY
+		# Move the character in the chosen direction
+		if move_direction :
+			velocity.x = move_direction.x * SPEED
+			velocity.z = move_direction.z * SPEED
 
-	# Move the character automatically every 3 seconds
-	if is_on_floor():
-		# Choose a random direction
-		var directions = [Vector3(1, 0, 0), Vector3(-1, 0, 0), Vector3(0, 0, 1), Vector3(0, 0, -1)]
-		var random_dir = directions[randi() % directions.size()]
-		move_direction = random_dir
-		print(move_direction)
-		
-		# Jump before moving
-		velocity.y = JUMP_VELOCITY
-	# Move the character in the chosen direction
-	if move_direction:
-		velocity.x = move_direction.x * SPEED
-		velocity.z = move_direction.z * SPEED
+		else:
+			velocity.x = move_toward(velocity.x, 0, SPEED)
+			velocity.z = move_toward(velocity.z, 0, SPEED)
+
+		move_and_slide()
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
-
-	move_and_slide()
+		velocity.x =0
+		velocity.y =0
+		velocity.z =0
+#  
